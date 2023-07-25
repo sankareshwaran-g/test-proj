c2ed7f0fe66de9b56286315e61e65ba946db0550
Sankareshwaran <128783508+sankareshwaran-g@users.noreply.github.com>
2023-07-25 14:47:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import HeroSection from './components/HeroSection'
+
+function App() {
+
+  return (
+    <>
+      <HeroSection/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section className='heroSection'>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-5">                        
+                        <div className="card mx-auto ">
+                            
+                            <div className="card-body">
+                            <h2 className=" text-xl-center fw-bold">IMPACT REPORT 2022</h2>
+                            </div>
+                            </div>                     
+ 
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default HeroSection
